5cf84fa04a6566ce437f3b04851a4d57f796bccb
villavargas <villa_vargas@hotmail.com>
2016-05-10 10:20:01 -0500

    fix error core data

--- VistasJerarquicas_OpenLibrary/AppDelegate.swift
+++ VistasJerarquicas_OpenLibrary/AppDelegate.swift
@@ -22,9 +22,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
         splitViewController.delegate = self
 
-        let masterNavigationController = splitViewController.viewControllers[0] as! UINavigationController
-        let controller = masterNavigationController.topViewController as! MasterViewController
-        controller.managedObjectContext = self.managedObjectContext
+        //let masterNavigationController = splitViewController.viewControllers[0] as! UINavigationController
+     //   let controller = masterNavigationController.topViewController as! MasterViewController
+       // controller.managedObjectContext = self.managedObjectContext
         return true
     }
 
@@ -49,7 +49,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
+       // self.saveContext()
     }
 
     // MARK: - Split view
@@ -77,17 +77,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         return NSManagedObjectModel(contentsOfURL: modelURL)!
     }()
 
-    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+ //   lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
         // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
         // Create the coordinator and store
-        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SingleViewCoreData.sqlite")
-        var failureReason = "There was an error creating or loading the application's saved data."
-        do {
+     //   let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+      //  let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SingleViewCoreData.sqlite")
+    //    var failureReason = "There was an error creating or loading the application's saved data."
+      /*  do {
             try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
-        } catch {
+        } catch {*/
             // Report any error we got.
-            var dict = [String: AnyObject]()
+         /*   var dict = [String: AnyObject]()
             dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
             dict[NSLocalizedFailureReasonErrorKey] = failureReason
 
@@ -97,22 +97,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
             // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
             abort()
-        }
+        }*/
         
-        return coordinator
-    }()
+   //     return coordinator
+    //}()
 
-    lazy var managedObjectContext: NSManagedObjectContext = {
+    //lazy var managedObjectContext: NSManagedObjectContext = {
         // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-        let coordinator = self.persistentStoreCoordinator
+      /*  let coordinator = self.persistentStoreCoordinator
         var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
         managedObjectContext.persistentStoreCoordinator = coordinator
         return managedObjectContext
-    }()
+    }()*/
 
     // MARK: - Core Data Saving support
 
-    func saveContext () {
+  /*  func saveContext () {
         if managedObjectContext.hasChanges {
             do {
                 try managedObjectContext.save()
@@ -124,7 +124,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
                 abort()
             }
         }
-    }
+    }*/
 
 }
 
